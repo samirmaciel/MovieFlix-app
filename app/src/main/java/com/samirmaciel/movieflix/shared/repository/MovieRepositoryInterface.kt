32955e9a92906d5c47819bf26a04549fd976e4e1
Samir Maciel <samir.maciel17@gmail.com>
2021-08-08 21:09:30 -0300

    setup language api

--- app/src/main/java/com/samirmaciel/movieflix/shared/repository/MovieRepositoryInterface.kt
+++ app/src/main/java/com/samirmaciel/movieflix/shared/repository/MovieRepositoryInterface.kt
@@ -7,10 +7,10 @@ import retrofit2.http.GET
 
 interface MovieRepositoryInterface{
 
-    @GET("/3/movie/popular?api_key=b2b2968f90966aca368661a132319376&language=pt-BR")
+    @GET("/3/movie/popular?api_key=b2b2968f90966aca368661a132319376&language=pt-BR&append_to_response=images&include_image_language=pt-BR,null")
     fun  getPopularMovies() : Call<MovieResponse>
 
-    @GET("/3/movie/top_rated?api_key=b2b2968f90966aca368661a132319376&language=pt-BR")
+    @GET("/3/movie/top_rated?api_key=b2b2968f90966aca368661a132319376&language=pt-BR&append_to_response=images&include_image_language=pt-BR,null")
     fun getRatedMovies() : Call<MovieResponse>
 
 }
